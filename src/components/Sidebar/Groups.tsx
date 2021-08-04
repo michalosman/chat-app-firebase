1ec08df1163dc7f85f1bef2ac38ad393f1a5300b
michalosman <michal.osman.997@gmail.com>
2021-08-04 20:24:21 +0200

    Clean code

--- src/components/Sidebar/Groups.tsx
+++ src/components/Sidebar/Groups.tsx
@@ -6,8 +6,8 @@ import db from '../../utils/db.json'
 
 const useStyles = makeStyles(() => ({
   avatar: {
-    height: '50px',
     width: '50px',
+    height: '50px',
   },
 
   scrollBox: {
--- src/components/Sidebar/Search.tsx
+++ src/components/Sidebar/Search.tsx
@@ -4,9 +4,9 @@ import { makeStyles } from '@material-ui/core/styles'
 
 const useStyles = makeStyles((theme) => ({
   input: {
-    borderRadius: '50px',
-    paddingLeft: theme.spacing(2),
     paddingRight: theme.spacing(2),
+    paddingLeft: theme.spacing(2),
+    borderRadius: '50px',
     backgroundColor: 'rgba(0, 0, 0, 0.05)',
   },
 }))
