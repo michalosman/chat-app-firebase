ed2b9d29600afdd71be0f0c53dcd382df02aea88
michalosman <michal.osman.997@gmail.com>
2021-08-18 20:12:26 +0200

    Add basic data fetch from firestore

--- src/App.tsx
+++ src/App.tsx
@@ -2,13 +2,36 @@ import Chat from './components/Chat'
 import Sidebar from './components/Sidebar'
 import Login from './components/Login'
 import { useAuthState } from 'react-firebase-hooks/auth'
-import { auth } from './firebase'
+import { useCollectionData } from 'react-firebase-hooks/firestore'
+import { auth, db } from './firebase'
 
 import { Box } from '@material-ui/core'
 import CircularProgress from '@material-ui/core/CircularProgress'
+import { useEffect } from 'react'
 
 const App = () => {
   const [user, loading] = useAuthState(auth)
+
+  const groupsQuery = db.collection('groups').orderBy('name', 'asc')
+  const [groups] = useCollectionData(groupsQuery, { idField: 'id' })
+
+  useEffect(() => {
+    if (user) {
+      console.log(user)
+      // set user
+    } else {
+      // reset user
+    }
+  }, [user])
+
+  useEffect(() => {
+    if (user) {
+      console.log(groups)
+      // set groups
+    } else {
+      // reset groups
+    }
+  }, [user, groups])
 
   return (
     <Box display="flex" height="100vh">
