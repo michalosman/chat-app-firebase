a29eec51e53a69e9a9a98a2758946bf412ef37f9
michalosman <michal.osman.997@gmail.com>
2021-08-23 17:24:59 +0200

    Add basic UserSearchbox functionality

--- src/components/UserSearchbox.tsx
+++ src/components/UserSearchbox.tsx
@@ -1,5 +1,9 @@
-import { Input, List, ListItem, makeStyles } from '@material-ui/core'
+import { Avatar, Input, List, ListItem, makeStyles } from '@material-ui/core'
 import { Dispatch, SetStateAction, useState } from 'react'
+import { useCollectionData } from 'react-firebase-hooks/firestore'
+import { db } from '../firebase'
+import User from '../types/User'
+import { convertUsers } from '../utils/converters'
 
 interface Props {
   setSelectedUserID: Dispatch<SetStateAction<string>>
@@ -14,11 +18,21 @@ const useStyles = makeStyles((theme) => ({
   },
 
   list: {
-    width: '100%',
-    maxHeight: '125px',
+    width: '105%',
+    height: '135px',
     padding: '0',
     borderRadius: '4px',
     overflow: 'scroll',
+  },
+
+  li: {
+    padding: theme.spacing(1),
+  },
+
+  avatar: {
+    width: theme.spacing(3),
+    height: theme.spacing(3),
+    marginRight: theme.spacing(1),
   },
 }))
 
@@ -26,9 +40,38 @@ const UserSearchbox = ({ setSelectedUserID }: Props) => {
   const classes = useStyles()
   const [input, setInput] = useState('')
 
+  const usersQuery = db.collection('users').orderBy('displayName')
+  const users = convertUsers(useCollectionData(usersQuery)[0])
+
   const handleInput = (e: React.ChangeEvent<HTMLInputElement>) => {
     setInput(e.currentTarget.value)
+    setSelectedUserID('')
   }
+
+  const filterResults = (word: string, users: User[]) => {
+    const regex = new RegExp(word, 'gi')
+    return users.filter((user) => user.displayName.match(regex))
+  }
+
+  const handleLiClick = (e: React.MouseEvent<HTMLDivElement, MouseEvent>) => {
+    if (e.currentTarget.textContent) setInput(e.currentTarget.textContent)
+    setSelectedUserID(e.currentTarget.id)
+  }
+
+  const usersList = users
+    ? filterResults(input, users).map((user) => (
+        <ListItem
+          key={user.uid}
+          id={user.uid}
+          className={classes.li}
+          button
+          onClick={handleLiClick}
+        >
+          <Avatar className={classes.avatar} src={user.photoURL} />
+          <div>{user.displayName}</div>
+        </ListItem>
+      ))
+    : []
 
   return (
     <div>
@@ -40,17 +83,12 @@ const UserSearchbox = ({ setSelectedUserID }: Props) => {
         disableUnderline
         required
       />
-      {input ? (
-        <List className={classes.list}>
-          <ListItem button>1</ListItem>
-          <ListItem button>2</ListItem>
-          <ListItem button>2</ListItem>
-          <ListItem button>2</ListItem>
-          <ListItem button>2</ListItem>
-        </List>
-      ) : (
-        ''
-      )}
+      <List className={classes.list}>
+        {usersList}
+        {usersList}
+        {usersList}
+        {usersList}
+      </List>
     </div>
   )
 }
