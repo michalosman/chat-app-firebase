912b46a07ef9065b1dd4c690306386e095526cd4
michalosman <michal.osman.997@gmail.com>
2021-08-24 16:40:51 +0200

    Add more  converters

--- src/utils/converters.ts
+++ src/utils/converters.ts
@@ -1,12 +1,42 @@
+import Group from '../types/Group'
+import Message from '../types/Message'
 import User from '../types/User'
 
-const convertUsers = (users: any): User[] => {
-  if (!users) return []
-
-  return users.map((userJSON: any) => {
-    const user: User = userJSON
-    return user
-  })
+const convertToUser = (userJSON: any): User => {
+  const user: User = userJSON
+  return user
 }
 
-export { convertUsers }
+const convertToGroup = (groupJSON: any): Group => {
+  const group: Group = groupJSON
+  return group
+}
+
+const convertToMessage = (messageJSON: any): Message => {
+  const message: Message = messageJSON
+  return message
+}
+
+const convertToUsers = (users: any): User[] => {
+  if (!users) return []
+  return users.map((userJSON: any) => convertToUser(userJSON))
+}
+
+const convertToGroups = (groups: any): Group[] => {
+  if (!groups) return []
+  return groups.map((groupJSON: any) => convertToGroup(groupJSON))
+}
+
+const convertToMessages = (messages: any): Group[] => {
+  if (!messages) return []
+  return messages.map((groupJSON: any) => convertToGroup(groupJSON))
+}
+
+export {
+  convertToUser,
+  convertToGroup,
+  convertToMessage,
+  convertToUsers,
+  convertToGroups,
+  convertToMessages,
+}
